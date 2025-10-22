8623a33ff57fed44de523025a836437c8ed2683c
Ayoub Dahir ali <69686810+AyoubDahir@users.noreply.github.com>
2025-10-22 16:49:11 +0300

    Update author name in __manifest__.py
    created by said dahir

--- idil/__manifest__.py
+++ idil/__manifest__.py
@@ -8,7 +8,7 @@
     "depends": ["mail", "point_of_sale", "web"],
     "application": True,
     "sequence": -100,
-    "author": "again",
+    "author": "Saciid DAHIR",
     "assets": {
         "web.assets_common": [
             # 'idil/static/src/scss/primary_variables.scss',
